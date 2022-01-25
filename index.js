111415e56505be66493196908d905b9e401f10d7
Admin <trsiebenhaar@gmail.com>
2022-01-25 14:59:12 -0700

    wip

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.post("/action", (req, res) => {
+app.post("/", (req, res) => {
   console.log("req: ", req.body);
   res.status(200);
   res.send("hello world");
