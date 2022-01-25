707214f92d539fe13c4071005dabf5fe8adff4e4
Admin <trsiebenhaar@gmail.com>
2022-01-25 14:50:49 -0700

    add req.body

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  console.log("req: ", req);
+  console.log("req: ", req.body);
   res.status(200);
   res.send("hello world");
 });
