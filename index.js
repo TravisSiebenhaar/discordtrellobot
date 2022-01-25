d98655e00572f91aaceba667c494d7fab2c91e49
Admin <trsiebenhaar@gmail.com>
2022-01-25 15:05:14 -0700

    wip

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.post("/", (req, res) => {
   console.log("req: ", req.body);
