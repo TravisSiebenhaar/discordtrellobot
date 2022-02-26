d50f9aebdf10aa31f804139390c88c50f9636c81
Admin <trsiebenhaar@gmail.com>
2022-02-25 23:40:32 -0700

    add client login

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.login(process.env.DISCORD_BOT_TOKEN);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
