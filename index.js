fb0d62abb2e1ac72431950fc8895ee6937da7e83
Admin <trsiebenhaar@gmail.com>
2022-02-25 23:35:54 -0700

    Add discord bot and trello response example.

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-import express from "express";
 import bodyParser from "body-parser";
+import express from "express";
+import dotenv from "dotenv";
+import { Client } from "discord.js";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const client = new Client({
+  intents: ["GUILDS", "GUILD_MESSAGES"],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.head("/", (req, res) => {
+  console.log("head requested");
+  res.status(200);
+  res.send("success");
+});
+
 app.post("/", (req, res) => {
-  console.log("req: ", req.body);
+  console.log("req: ", req.body.action.display);
   res.status(200);
   res.send("hello world");
 });
---
+++ trello_res.example.js
@@ -0,0 +1,201 @@
+req: {
+  model: {
+    id: '61de01c0fd9d2b35d229b95f',
+    name: 'Project Management',
+    desc: '**Manage Projects To Perfection**\n' +
+      '\n' +
+      "Big dreams turn into bigger results with a killer project plan. Use this basic structure to build your team's ideal workflow, for projects big or small.\n" +
+      '\n' +
+      "Here's what's included, and how to get this project management party started:\n" +
+      '\n' +
+      '1. **Resources (Start Here!):**\n' +
+      'This list is perfect for you and your team to have all those documents, specs, designs, or anything you tend to reference all the time. Meeting schedules and answers to common questions are also recommended to pop into this list so that your team is informed at all times. 📚\n' +
+      '\n' +
+      '2. **Questions For Next Meeting:**\n' +
+      "Never again forget that important question you thought of in the shower this morning! Slide your Q's into this handy list so your team keeps on flowing. 🌊\n" +
+      '\n' +
+      '3. **To Do:**\n' +
+      "The mother of all lists—the good ol' To Do. This is where assigned tasks live so that your team can see who's working on what and when it's due. Crystal clear all day, week, and month. 🔮\n" +
+      '\n' +
+      '4. **Pending:**\n' +
+      "We all have those in-between tasks that are technically done but also awkwardly waiting for another step. That's where the Pending list comes into play, use this list to share what those next mini-steps are with your team. 💬\n" +
+      '\n' +
+      '5. **Blocked:**\n' +
+      'With any successful project comes at least one inevitable blocker. Splash those red-tape, blocking issues that are slowing your team down so you can tackle it in the best way; together. 🚷\n' +
+      '\n' +
+      '6. **Done:**\n' +
+      'Our favorite list, the Done list. Stop, drag, and drop! For all your finished tasks that your team has hustled on and feels proud of. ✨',
+    descData: null,
+    closed: false,
+    idOrganization: '61de0191e775570b843382bb',
+    idEnterprise: null,
+    pinned: false,
+    url: 'https://trello.com/b/dXbgTWRD/project-management',
+    shortUrl: 'https://trello.com/b/dXbgTWRD',
+    prefs: {
+      permissionLevel: 'org',
+      hideVotes: false,
+      voting: 'disabled',
+      comments: 'members',
+      invitations: 'members',
+      selfJoin: true,
+      cardCovers: true,
+      isTemplate: false,
+      cardAging: 'regular',
+      calendarFeedEnabled: false,
+      hiddenPluginBoardButtons: [],
+      background: 'grey',
+      backgroundColor: '#838C91',
+      backgroundImage: null,
+      backgroundImageScaled: null,
+      backgroundTile: false,
+      backgroundBrightness: 'dark',
+      backgroundBottomColor: '#838C91',
+      backgroundTopColor: '#838C91',
+      canBePublic: true,
+      canBeEnterprise: true,
+      canBeOrg: true,
+      canBePrivate: true,
+      canInvite: true
+    },
+    labelNames: {
+      green: '',
+      yellow: 'Copy Request',
+      orange: 'One more step',
+      red: 'Priority',
+      purple: 'Design Team',
+      blue: 'Product Marketing',
+      sky: 'Trello Tip',
+      lime: 'Halp',
+      pink: '',
+      black: ''
+    }
+  },
+  action: {
+    id: '62170652953b1881efdaa770',
+    idMemberCreator: '61de017d59ce787c4ab8b41e',
+    data: {
+      card: [Object],
+      old: [Object],
+      board: [Object],
+      listBefore: [Object],
+      listAfter: [Object]
+    },
+    appCreator: null,
+    type: 'updateCard',
+    date: '2022-02-24T04:15:14.033Z',
+    limits: {},
+    display: {
+      translationKey: 'action_move_card_from_list_to_list',
+      entities: [Object]
+    },
+    memberCreator: {
+      id: '61de017d59ce787c4ab8b41e',
+      activityBlocked: false,
+      avatarHash: '7b684e8b017dda3bc90e089bf5aa268e',
+      avatarUrl: 'https://trello-members.s3.amazonaws.com/61de017d59ce787c4ab8b41e/7b684e8b017dda3bc90e089bf5aa268e',
+      fullName: 'Travis Siebenhaar',
+      idMemberReferrer: null,
+      initials: 'TS',
+      nonPublic: {},
+      nonPublicAvailable: true,
+      username: 'travissiebenhaar2'
+    }
+  }
+}
+req:  {
+  model: {
+    id: '61de01c0fd9d2b35d229b95f',
+    name: 'Project Management',
+    desc: '**Manage Projects To Perfection**\n' +
+      '\n' +
+      "Big dreams turn into bigger results with a killer project plan. Use this basic structure to build your team's ideal workflow, for projects big or small.\n" +
+      '\n' +
+      "Here's what's included, and how to get this project management party started:\n" +
+      '\n' +
+      '1. **Resources (Start Here!):**\n' +
+      'This list is perfect for you and your team to have all those documents, specs, designs, or anything you tend to reference all the time. Meeting schedules and answers to common questions are also recommended to pop into this list so that your team is informed at all times. 📚\n' +
+      '\n' +
+      '2. **Questions For Next Meeting:**\n' +
+      "Never again forget that important question you thought of in the shower this morning! Slide your Q's into this handy list so your team keeps on flowing. 🌊\n" +
+      '\n' +
+      '3. **To Do:**\n' +
+      "The mother of all lists—the good ol' To Do. This is where assigned tasks live so that your team can see who's working on what and when it's due. Crystal clear all day, week, and month. 🔮\n" +
+      '\n' +
+      '4. **Pending:**\n' +
+      "We all have those in-between tasks that are technically done but also awkwardly waiting for another step. That's where the Pending list comes into play, use this list to share what those next mini-steps are with your team. 💬\n" +
+      '\n' +
+      '5. **Blocked:**\n' +
+      'With any successful project comes at least one inevitable blocker. Splash those red-tape, blocking issues that are slowing your team down so you can tackle it in the best way; together. 🚷\n' +
+      '\n' +
+      '6. **Done:**\n' +
+      'Our favorite list, the Done list. Stop, drag, and drop! For all your finished tasks that your team has hustled on and feels proud of. ✨',
+    descData: null,
+    closed: false,
+    idOrganization: '61de0191e775570b843382bb',
+    idEnterprise: null,
+    pinned: false,
+    url: 'https://trello.com/b/dXbgTWRD/project-management',
+    shortUrl: 'https://trello.com/b/dXbgTWRD',
+    prefs: {
+      permissionLevel: 'org',
+      hideVotes: false,
+      voting: 'disabled',
+      comments: 'members',
+      invitations: 'members',
+      selfJoin: true,
+      cardCovers: true,
+      isTemplate: false,
+      cardAging: 'regular',
+      calendarFeedEnabled: false,
+      hiddenPluginBoardButtons: [],
+      background: 'grey',
+      backgroundColor: '#838C91',
+      backgroundImage: null,
+      backgroundImageScaled: null,
+      backgroundTile: false,
+      backgroundBrightness: 'dark',
+      backgroundBottomColor: '#838C91',
+      backgroundTopColor: '#838C91',
+      canBePublic: true,
+      canBeEnterprise: true,
+      canBeOrg: true,
+      canBePrivate: true,
+      canInvite: true
+    },
+    labelNames: {
+      green: '',
+      yellow: 'Copy Request',
+      orange: 'One more step',
+      red: 'Priority',
+      purple: 'Design Team',
+      blue: 'Product Marketing',
+      sky: 'Trello Tip',
+      lime: 'Halp',
+      pink: '',
+      black: ''
+    }
+  },
+  action: {
+    id: '62170652953b1881efdaa780',
+    idMemberCreator: '61de017d59ce787c4ab8b41e',
+    data: { card: [Object], old: [Object], board: [Object], list: [Object] },
+    appCreator: null,
+    type: 'updateCard',
+    date: '2022-02-24T04:15:14.052Z',
+    limits: {},
+    display: { translationKey: 'action_moved_card_lower', entities: [Object] },
+    memberCreator: {
+      id: '61de017d59ce787c4ab8b41e',
+      activityBlocked: false,
+      avatarHash: '7b684e8b017dda3bc90e089bf5aa268e',
+      avatarUrl: 'https://trello-members.s3.amazonaws.com/61de017d59ce787c4ab8b41e/7b684e8b017dda3bc90e089bf5aa268e',
+      fullName: 'Travis Siebenhaar',
+      idMemberReferrer: null,
+      initials: 'TS',
+      nonPublic: {},
+      nonPublicAvailable: true,
+      username: 'travissiebenhaar2'
+    }
+  }
+}
